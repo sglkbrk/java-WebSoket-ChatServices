905915d6b99154f5f5bc68532f977173096e22c4
burak <sglk.brk@gmail.com>
2021-02-10 12:30:44 +0300

    dz

--- src/main/java/com/fivoriteam/chatservice/configuration/WebSocketConfig.java
+++ src/main/java/com/fivoriteam/chatservice/configuration/WebSocketConfig.java
@@ -35,7 +35,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS().setClientLibraryUrl("https://cdn.jsdelivr.net/sockjs/latest/sockjs.min.js");
     }
 
     @Override
